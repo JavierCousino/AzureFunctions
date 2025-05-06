7b997e05824144a01363fff9eea5b7a60dfdd839
Fco. Javier Cousiño <80127448+JavierCousino@users.noreply.github.com>
2025-05-06 16:45:12 +0000

    Create azure function

---
+++ HelloHttpTrigger.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Function
+{
+    public class HelloHttpTrigger
+    {
+        private readonly ILogger<HelloHttpTrigger> _logger;
+
+        public HelloHttpTrigger(ILogger<HelloHttpTrigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HelloHttpTrigger")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
